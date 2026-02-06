4437aca10b86340ac93ed6a1fbad0133c5a0e621
ZdapX <stokmlmurmer150@gmail.com>
2026-02-06 11:11:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,43 +4,56 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+// KONFIGURASI PENTING
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views')); // Mengunci lokasi folder views
+app.use(express.static(path.join(__dirname, 'public'))); // Mengunci lokasi folder public
 
-// Gunakan Objek agar lebih mudah dikelola berdasarkan Socket ID
 let outputDevices = {};
 
-app.get('/', (req, res) => res.render('index'));
-app.get('/output', (req, res) => res.render('output'));
-app.get('/control', (req, res) => res.render('control'));
+// ROUTING
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
+app.get('/output', (req, res) => {
+    res.render('output');
+});
+
+app.get('/control', (req, res) => {
+    res.render('control');
+});
+
+// SOCKET.IO LOGIC
 io.on('connection', (socket) => {
-    console.log('Koneksi baru:', socket.id);
+    console.log('User terhubung:', socket.id);
 
-    // Kirim list device yang ada saat ini ke siapa pun yang baru konek (terutama controller)
-    socket.emit('update-device-list', Object.values(outputDevices));
-
-    // Registrasi device sebagai output
     socket.on('register-output', (deviceName) => {
         outputDevices[socket.id] = { id: socket.id, name: deviceName };
-        // Beritahu semua orang bahwa ada device baru
         io.emit('update-device-list', Object.values(outputDevices));
-        console.log(`Device terdaftar: ${deviceName}`);
     });
 
-    // Kontrol suara (Play/Pause)
+    socket.on('get-devices', () => {
+        socket.emit('update-device-list', Object.values(outputDevices));
+    });
+
     socket.on('send-command', (data) => {
         io.to(data.targetId).emit('audio-control', data.action);
     });
 
     socket.on('disconnect', () => {
         if (outputDevices[socket.id]) {
-            console.log('Device keluar:', outputDevices[socket.id].name);
             delete outputDevices[socket.id];
             io.emit('update-device-list', Object.values(outputDevices));
         }
     });
 });
 
+// RUN SERVER
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => {
+    console.log(`Server Berhasil Jalan!`);
+    console.log(`Buka di browser: http://localhost:${PORT}`);
+}).on('error', (err) => {
+    console.error('Gagal menjalankan server:', err.message);
+});
